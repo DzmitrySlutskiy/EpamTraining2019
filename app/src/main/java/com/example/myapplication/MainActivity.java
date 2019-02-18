f227a4daf931d8f160214af68fe2c0939fb8666f
Dzmitry Slutski <Dzmitry_Slutski@epam.com>
2019-02-18 19:11:34 +0300

    second toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Simple toast", Toast.LENGTH_SHORT).show();
-
-        Toast.makeText(this, "This is second branch", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Simple toast for feature 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "This is second branch second toast", Toast.LENGTH_SHORT).show();
     }
 }
