0e9de0dd598f6c534437be0be14b0cea9fe5ded1
Dzmitry Slutski <Dzmitry_Slutski@epam.com>
2019-02-18 19:13:53 +0300

    second toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Simple toast", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Simple toast changed", Toast.LENGTH_SHORT).show();
     }
 }
