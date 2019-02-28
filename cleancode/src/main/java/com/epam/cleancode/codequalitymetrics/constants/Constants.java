e0eaafc8048224b0a7fcebaf2fa5297bd17e9680
Yahor Urbanovich <yahor_urbanovich@gmail.com>
2019-02-28 14:54:37 +0300

    Remove code quality homework

--- cleancode/src/main/java/com/epam/cleancode/codequalitymetrics/constants/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.epam.cleancode.codequalitymetrics.constants;
-
-public class Constants {
-    public final static String FILE_DATA_SEPERATOR = ":";
-}
--- cleancode/src/main/java/com/epam/cleancode/codequalitymetrics/exception/InvalidDataException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.cleancode.codequalitymetrics.exception;
-
-public class InvalidDataException extends RuntimeException{
-
-    public InvalidDataException(String exception) {
-        super(exception);
-    }
-
-}
