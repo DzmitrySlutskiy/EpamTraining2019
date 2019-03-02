9d2cae075d0dbd82107b2388c5e7243d313cd4f5
Aliaksei Shvants <ashvants91@gmail.com>
2019-03-03 01:12:03 +0300

    refactored 'comments' task

--- cleancode/src/main/java/com/epam/cleancode/comments/InvalidInputException.java
+++ cleancode/src/main/java/com/epam/cleancode/comments/InvalidInputException.java
@@ -1,16 +1,16 @@
 package com.epam.cleancode.comments;
 
-public class InvalidInputException extends RuntimeException{
-    String message;
-    public InvalidInputException(String s) {
-        super();
-        this.message = s;
+public class InvalidInputException extends RuntimeException {
+    private final String message;
 
+    public InvalidInputException(final String message) {
+        super();
+
+        this.message = message;
     }
 
     @Override
     public String getMessage() {
         return message;
     }
-
 }
