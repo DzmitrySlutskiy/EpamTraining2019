753e29340a97d4f453026bb7f868cbcef7c355fa
Aleksei <aleksei.konopeshko@gmail.com>
2019-03-03 11:46:07 +0300

    Format according to code style

--- cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
+++ cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
@@ -2,38 +2,19 @@ package com.epam.cleancode.comments;
 
 public class MortgageInstallmentCalculator {
 
-    /**
-     *
-     * @param p principal amount
-     * @param t term of mortgage in years
-     * @param r rate of interest
-     * @return monthly payment amount
-     */
-    public static double calculateMonthlyPayment(
-            int p, int t, double r) {
-
-        //cannot have negative loanAmount, term duration and rate of interest
-        if (p < 0 || t <= 0 || r < 0) {
+    public static double calculateMonthlyPayment(int principal, int mortgageTermInYears, double rateOfInterest) {
+        if (principal < 0 || mortgageTermInYears <= 0 || rateOfInterest < 0) {
             throw new InvalidInputException("Negative values are not allowed");
         }
 
-        // Convert interest rate into a decimal - eg. 6.5% = 0.065
-        r /= 100.0;
+        rateOfInterest /= 100.0;
+        double mortgageTermInMonths = mortgageTermInYears * 12;
+        double monthlyRateOfInterest = rateOfInterest / 12.0;
 
-        // convert term in years to term in months
-        double tim = t * 12;
+        if (rateOfInterest == 0) {
+            return principal / mortgageTermInMonths;
+        }
 
-        //for zero interest rates
-        if(r==0)
-            return  p/tim;
-
-        // convert into monthly rate
-        double m = r / 12.0;
-
-        // Calculate the monthly payment
-        // The Math.pow() method is used calculate values raised to a power
-        double monthlyPayment = (p * m) / (1 - Math.pow(1 + m, -tim));
-
-        return monthlyPayment;
+        return (principal * monthlyRateOfInterest) / (1 - Math.pow(1 + monthlyRateOfInterest, -mortgageTermInMonths));
     }
 }
--- cleancode/src/main/java/com/epam/cleancode/singleresponsibility/WrongFormatException.java
+++ cleancode/src/main/java/com/epam/cleancode/singleresponsibility/WrongFormatException.java
@@ -1,4 +1,17 @@
 package com.epam.cleancode.singleresponsibility;
 
 public class WrongFormatException extends RuntimeException {
+
+    private String message;
+
+    public WrongFormatException(String msg) {
+        super();
+        message = msg;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
 }
