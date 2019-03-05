c5ccd3375343e22439dd4d31bb42808cd1b32204
Maxim <zheemax@gmail.com>
2019-03-05 23:34:41 +0300

    edited according to code style guidelines

--- cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
+++ cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
@@ -2,38 +2,24 @@ package com.epam.cleancode.comments;
 
 public class MortgageInstallmentCalculator {
 
-    /**
-     *
-     * @param p principal amount
-     * @param t term of mortgage in years
-     * @param r rate of interest
-     * @return monthly payment amount
-     */
-    public static double calculateMonthlyPayment(
-            int p, int t, double r) {
 
-        //cannot have negative loanAmount, term duration and rate of interest
-        if (p < 0 || t <= 0 || r < 0) {
+    public static double calculateMonthlyPayment(
+            int PrincipalAmount,
+            int TermOfMortageInYears,
+            double RateOfInterest) {
+
+        if (PrincipalAmount < 0 || TermOfMortageInYears <= 0 || RateOfInterest < 0) {
             throw new InvalidInputException("Negative values are not allowed");
         }
 
-        // Convert interest rate into a decimal - eg. 6.5% = 0.065
-        r /= 100.0;
+        RateOfInterest /= 100.0;
 
-        // convert term in years to term in months
-        double tim = t * 12;
+        double TermInMonth = TermOfMortageInYears * 12;
 
-        //for zero interest rates
-        if(r==0)
-            return  p/tim;
-
-        // convert into monthly rate
-        double m = r / 12.0;
-
-        // Calculate the monthly payment
-        // The Math.pow() method is used calculate values raised to a power
-        double monthlyPayment = (p * m) / (1 - Math.pow(1 + m, -tim));
-
+        if(RateOfInterest==0)
+            return  PrincipalAmount/TermInMonth;
+        double MonthlyRate = RateOfInterest / 12.0;
+        double monthlyPayment = (PrincipalAmount * MonthlyRate) / (1 - Math.pow(1 + MonthlyRate, -TermInMonth));
         return monthlyPayment;
     }
 }
