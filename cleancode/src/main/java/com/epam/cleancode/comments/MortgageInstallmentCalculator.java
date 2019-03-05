1381b7377c1993c7b4022cd8730fb6151d60697e
AlekseichukA <alekseichuk4@gmail.com>
2019-03-05 09:36:34 +0300

    I made some changes

--- cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
+++ cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
@@ -2,37 +2,26 @@ package com.epam.cleancode.comments;
 
 public class MortgageInstallmentCalculator {
 
+    private static final String MESSAGE_BAD_INPUT_PARAMETERS = "Negative values are not allowed";
+
     /**
-     *
-     * @param p principal amount
-     * @param t term of mortgage in years
-     * @param r rate of interest
+     * @param startAmount principal amount
+     * @param termInYears term of mortgage in years
+     * @param percent rate of interest
      * @return monthly payment amount
      */
-    public static double calculateMonthlyPayment(
-            int p, int t, double r) {
-
-        //cannot have negative loanAmount, term duration and rate of interest
-        if (p < 0 || t <= 0 || r < 0) {
-            throw new InvalidInputException("Negative values are not allowed");
+    public static double calculateMonthlyPayment(int startAmount, int termInYears, double percent) {
+        if (startAmount < 0 || termInYears <= 0 || percent < 0) {
+            throw new InvalidInputException(MESSAGE_BAD_INPUT_PARAMETERS);
         }
-
-        // Convert interest rate into a decimal - eg. 6.5% = 0.065
-        r /= 100.0;
-
-        // convert term in years to term in months
-        double tim = t * 12;
-
-        //for zero interest rates
-        if(r==0)
-            return  p/tim;
-
-        // convert into monthly rate
-        double m = r / 12.0;
-
-        // Calculate the monthly payment
-        // The Math.pow() method is used calculate values raised to a power
-        double monthlyPayment = (p * m) / (1 - Math.pow(1 + m, -tim));
+        double percentInYear = percent / 100.0;
+        double termInMonth = termInYears * 12;
+        if (percentInYear == 0) {
+            return startAmount / termInMonth;
+        }
+        double percentInMonth = percentInYear / 12.0;
+        double monthlyPayment = (startAmount * percentInMonth) /
+                (1 - Math.pow(1 + percentInMonth, -termInMonth));
 
         return monthlyPayment;
     }
--- cleancode/src/main/java/com/epam/cleancode/singleresponsibility/WrongFormatException.java
+++ cleancode/src/main/java/com/epam/cleancode/singleresponsibility/WrongFormatException.java
@@ -1,4 +1,16 @@
 package com.epam.cleancode.singleresponsibility;
 
 public class WrongFormatException extends RuntimeException {
+
+    private String message;
+
+    public WrongFormatException(String s) {
+        super();
+        this.message = s;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
