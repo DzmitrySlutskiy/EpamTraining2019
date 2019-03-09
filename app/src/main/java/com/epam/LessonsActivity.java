62632ae32d277e6ed246bf2b578a405d7bc10aa9
Aliaksei Shvants <ashvants91@gmail.com>
2019-03-09 17:15:48 +0300

    made first part: added lessons to 'Epam Training' app

--- app/src/main/java/com/epam/LessonsActivity.java
+++ app/src/main/java/com/epam/LessonsActivity.java
@@ -5,29 +5,81 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.epam.cleancodetest.R;
 import com.epam.themes.androidcomponents.AndroidComponentsActivity;
+import com.epam.themes.cleancode.CleanCodeActivity;
 import com.epam.themes.uicomponents.UIComponentsActivity;
 
 public class LessonsActivity extends AppCompatActivity {
 
+    private ViewGroup firstLesson;
+    private ViewGroup secondLesson;
+    private ViewGroup thirdLesson;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons);
 
-        findViewById(R.id.lesson_components_root_view).setOnClickListener(new View.OnClickListener() {
+        firstLesson = findViewById(R.id.fist_lesson);
+        secondLesson = findViewById(R.id.second_lesson);
+        thirdLesson = findViewById(R.id.third_lesson);
+
+        setLessonParameters(firstLesson, R.drawable.ic_mentor_face, R.string.mentor_name_yahor,
+                R.string.theme_clean_code, R.string.first_lesson_date);
+
+        setLessonParameters(secondLesson, R.drawable.ic_account_circle, R.string.mentor_name_arziom,
+                R.string.theme_android_components, R.string.second_lesson_date);
+
+        setLessonParameters(thirdLesson, R.drawable.ic_mentor_face, R.string.mentor_name_yahor,
+                R.string.theme_android_ui, R.string.third_lesson_date);
+
+        findViewById(R.id.fist_lesson).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(CleanCodeActivity.class);
+            }
+        });
+
+        findViewById(R.id.second_lesson).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(AndroidComponentsActivity.class);
+            }
+        });
+
+        findViewById(R.id.third_lesson).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openLesson(UIComponentsActivity.class);
             }
         });
 
-        //todo open ui activity
     }
 
-    private void openLesson(Class<? extends Activity> lessonActivityClass) {
+    private void setLessonParameters(final ViewGroup lessonItem,
+                                     final int icon,
+                                     final int name,
+                                     final int theme,
+                                     final int date) {
+        ((ImageView) lessonItem.findViewById(R.id.android_components_mentor_image_view))
+                .setImageResource(icon);
+
+        ((TextView) lessonItem.findViewById(R.id.android_components_mentor_text_view))
+                .setText(name);
+
+        ((TextView) lessonItem.findViewById(R.id.android_components_lesson_theme))
+                .setText(theme);
+
+        ((TextView) lessonItem.findViewById(R.id.android_components_lesson_date))
+                .setText(date);
+    }
+
+    private void openLesson(final Class<? extends Activity> lessonActivityClass) {
         startActivity(new Intent(this, lessonActivityClass));
     }
 }
---
+++ app/src/main/java/com/epam/themes/cleancode/CleanCodeActivity.java
@@ -0,0 +1,15 @@
+package com.epam.themes.cleancode;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.epam.cleancodetest.R;
+
+public class CleanCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clean_code);
+    }
+}
