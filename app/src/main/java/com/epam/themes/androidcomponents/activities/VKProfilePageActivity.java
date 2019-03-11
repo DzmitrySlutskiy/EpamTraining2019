a0dc6e0fb78edeca50c96a026737ddd6aea2bc87
wanderer005 <wanderer005@gmail.com>
2019-03-11 11:09:55 +0300

    HomeWork 06.03. Android View Components Part 2.1: VK profile page design

--- app/src/main/java/com/epam/themes/androidcomponents/activities/VKProfilePageActivity.java
+++ app/src/main/java/com/epam/themes/androidcomponents/activities/VKProfilePageActivity.java
@@ -3,8 +3,12 @@ package com.epam.themes.androidcomponents.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageButton;
 
 import com.epam.cleancodetest.R;
+
+import java.lang.reflect.Field;
 
 public class VKProfilePageActivity extends AppCompatActivity {
 
@@ -13,7 +17,15 @@ public class VKProfilePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vkprofile_page);
 
-        Toolbar toolbar = findViewById(R.id.vk_toolbar);
+        Toolbar toolbar = findViewById(R.id.vk_toolbar_root);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        toolbar.setLogo(R.drawable.ic_keyboard_backspace_white_24dp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_vk_profile, menu);
+        return true;
     }
 }
