0fc7c905e9700eadae51591065d7dab4db727a9e
salozhor <berdnikov_ev1_16@student.grsu.by>
2019-03-12 15:40:49 +0300

    Made code styling

--- cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
+++ cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
@@ -1,39 +1,22 @@
 package com.epam.cleancode.comments;
 
 public class MortgageInstallmentCalculator {
+    public static double calculateMonthlyPayment(int loanAmount, int termDuration,
+                                                 double percentBasedRateOfInterest) {
 
-    /**
-     *
-     * @param p principal amount
-     * @param t term of mortgage in years
-     * @param r rate of interest
-     * @return monthly payment amount
-     */
-    public static double calculateMonthlyPayment(
-            int p, int t, double r) {
-
-        //cannot have negative loanAmount, term duration and rate of interest
-        if (p < 0 || t <= 0 || r < 0) {
+        if (loanAmount < 0 || termDuration <= 0 || percentBasedRateOfInterest < 0) {
             throw new InvalidInputException("Negative values are not allowed");
         }
 
-        // Convert interest rate into a decimal - eg. 6.5% = 0.065
-        r /= 100.0;
+        double decimalRateOfInterest = percentBasedRateOfInterest / 100.0;
+        double termDurationInMonths = termDuration * 12.0;
 
-        // convert term in years to term in months
-        double tim = t * 12;
+        if(decimalRateOfInterest==0) {
+            return loanAmount / termDurationInMonths;
+        }
 
-        //for zero interest rates
-        if(r==0)
-            return  p/tim;
+        double monthlyRate = decimalRateOfInterest / 12.0;
 
-        // convert into monthly rate
-        double m = r / 12.0;
-
-        // Calculate the monthly payment
-        // The Math.pow() method is used calculate values raised to a power
-        double monthlyPayment = (p * m) / (1 - Math.pow(1 + m, -tim));
-
-        return monthlyPayment;
+        return (loanAmount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -termDurationInMonths));
     }
 }
