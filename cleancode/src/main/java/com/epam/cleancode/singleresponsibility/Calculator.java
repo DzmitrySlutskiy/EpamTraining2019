d9e383e125b6f9c452c30ba7c60d9eeb86250249
witaless <witaless@users.noreply.github.com>
2019-03-14 03:21:14 +0300

    improved code style according to clean code

--- cleancode/src/main/java/com/epam/cleancode/singleresponsibility/Calculator.java
+++ cleancode/src/main/java/com/epam/cleancode/singleresponsibility/Calculator.java
@@ -1,48 +1,69 @@
 package com.epam.cleancode.singleresponsibility;
 
-public class Calculator{
+public class Calculator {
 
     private static final String SEPARATOR = ",";
+    private static final String VALIDATION_EXCEPTION_MESSAGE = "Only digits allowed";
+    private static final String REGEX_FOR_IS_DIGITS_MATCHING = "[\\d" + SEPARATOR + "]*";
 
-    public String add(String numbers) {
+    public String add(final String numbers) {
         validate(numbers);
+
         return "sum: " + String.valueOf(getSum(numbers));
     }
 
-    public String multiply(String numbers) {
+    public String multiply(final String numbers) {
         validate(numbers);
+
         return "product: " + String.valueOf(getProduct(numbers));
     }
 
-    private int getSum(String numbers) {
+    private int getSum(final String numbers) {
         int sum = 0;
-        for (String s : numbers.split(SEPARATOR)) if (isNotEmpty(s)) sum += Integer.valueOf(s);
+
+        for (String s : numbers.split(SEPARATOR)) {
+
+            if (isNotEmpty(s)) {
+                sum += Integer.valueOf(s);
+            }
+
+        }
+
         return sum;
     }
 
-    private int getProduct(String numbers) {
+    private int getProduct(final String numbers) {
         int sum = 1;
-        for (String s : numbers.split(SEPARATOR))
-            if (isNotEmpty(s))
+
+        for (String s : numbers.split(SEPARATOR)) {
+
+            if (isNotEmpty(s)) {
                 sum *= Integer.valueOf(s);
+            }
+
+        }
+
         return sum;
     }
 
-    private boolean isNotEmpty(String numbers) {
+    private boolean isNotEmpty(final String numbers) {
         return !numbers.isEmpty();
     }
 
-    private void validate(String numbers) {
-        if (numbers == null || isNotDigits(numbers))
-            throw new WrongFormatException();
+    private void validate(final String numbers) {
+
+        if (numbers == null || isNotDigits(numbers)) {
+            throw new WrongFormatException(VALIDATION_EXCEPTION_MESSAGE);
+        }
+
     }
 
-    private boolean isNotDigits(String numbers) {
+    private boolean isNotDigits(final String numbers) {
         return !isDigits(numbers);
     }
 
-    private boolean isDigits(String numbers) {
-        return numbers.matches("[\\d" + SEPARATOR + "]*");
+    private boolean isDigits(final String numbers) {
+        return numbers.matches(REGEX_FOR_IS_DIGITS_MATCHING);
     }
 
 }
--- cleancode/src/main/java/com/epam/cleancode/singleresponsibility/WrongFormatException.java
+++ cleancode/src/main/java/com/epam/cleancode/singleresponsibility/WrongFormatException.java
@@ -1,4 +1,16 @@
 package com.epam.cleancode.singleresponsibility;
 
 public class WrongFormatException extends RuntimeException {
+    private final String message;
+
+    public WrongFormatException(String exceptionMessage) {
+        super();
+
+        message = exceptionMessage;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
