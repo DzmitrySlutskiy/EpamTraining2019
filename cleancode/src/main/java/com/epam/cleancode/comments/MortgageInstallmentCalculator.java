a491cf0f189248a2fb48a7fb62e0326b0855a1cb
AlekseichukA <alekseichuk4@gmail.com>
2019-03-18 00:20:15 +0300

    I corrected your comments

--- cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
+++ cleancode/src/main/java/com/epam/cleancode/comments/MortgageInstallmentCalculator.java
@@ -6,11 +6,13 @@ public class MortgageInstallmentCalculator {
     private static final int AMOUNT_MONTHS_IN_YEAR = 12;
 
     public static double calculateMonthlyPayment(int startAmount, int termInYears, double percent) {
+        inputValidation(startAmount, termInYears, percent);
         if (haveValidParameters(startAmount, termInYears, percent)) {
             throw new InvalidInputException(MESSAGE_BAD_INPUT_PARAMETERS);
         }
         double percentInYear = percent / 100.0;
         double termInMonth = termInYears * AMOUNT_MONTHS_IN_YEAR;
+
         if (percentInYear == 0) {
             return startAmount / termInMonth;
         }
@@ -18,12 +20,13 @@ public class MortgageInstallmentCalculator {
         return monthlyPayment(startAmount, percentInYear, termInMonth);
     }
 
-    private static boolean haveValidParameters(int startAmount, int termInYears, double percent) {
-        return startAmount < 0 || termInYears <= 0 || percent < 0;
+    private static void inputValidation(int startAmount, int termInYears, double percent) {
+        if (startAmount < 0 || termInYears <= 0 || percent < 0) {
+        throw new InvalidInputException(MESSAGE_BAD_INPUT_PARAMETERS);
+        }
     }
 
-    private static double monthlyPayment(int startAmount, double percentInYear,
-                                         double termInMonth) {
+    private static double monthlyPayment(int startAmount, double percentInYear) {
         return (startAmount * percentInYear / AMOUNT_MONTHS_IN_YEAR) /
                 (1 - Math.pow(1 + percentInYear / AMOUNT_MONTHS_IN_YEAR, -termInMonth));
     }
