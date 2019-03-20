a78dcb6eea2b1b1da2709f5169c7e24910983e2b
sanek <saneklic@gmail.com>
2019-03-20 10:12:33 +0300

    complete homework 18.03.2019

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/MyDate.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/MyDate.kt
@@ -12,14 +12,32 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     }
 }
 
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) = addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.number)
+
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR;
+
+    operator fun times(number: Int) = RepeatedTimeInterval(this, number)
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate){
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterator<MyDate> {
+    var date: MyDate = start
+
+    override fun hasNext() = date <= endInclusive
+
+    override fun next(): MyDate {
+        val resultDate = date
+        date = date.nextDay()
+        return resultDate
+    }
+
     operator fun contains(d: MyDate) = d >= start && d <= endInclusive
 }
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
