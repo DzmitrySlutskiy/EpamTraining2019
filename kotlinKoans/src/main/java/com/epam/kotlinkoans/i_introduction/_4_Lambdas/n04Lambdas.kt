5002353575d62ac83c52f00afe14eedfe6203a9b
grodfor <vlad.sitsko@gmail.com>
2019-03-21 01:44:58 +0300

    completed introduction

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -1,6 +1,8 @@
 package i_introduction._4_Lambdas
 
 import com.epam.kotlinkoans.i_introduction._4_Lambdas.JavaCode4
+import com.google.common.base.Predicate
+import com.google.common.collect.Iterables
 import util.TODO
 import util.doc4
 
@@ -23,4 +25,4 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean = collection.any { element -> element % 2 == 0 }
