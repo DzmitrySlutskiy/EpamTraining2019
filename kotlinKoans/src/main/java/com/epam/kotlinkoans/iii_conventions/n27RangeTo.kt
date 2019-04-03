e1e021df04298d36784084ea8f371c33ec4243a5
AlexGolovach <alexgolovach1998@gmail.com>
2019-04-03 23:43:45 +0300

    Completed conventions

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/n27RangeTo.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/n27RangeTo.kt
@@ -1,20 +1,3 @@
 package iii_conventions
 
-import util.TODO
-import util.doc26
-
-fun todoTask27(): Nothing = TODO(
-    """
-        Task 27.
-        Uncomment the commented code and make it compile.
-        Make all the changes to the file MyDate.kt.
-
-        Tips: To make '..' work implement a 'MyDate.rangeTo()' extension function returning 'DateRange'.
-    """,
-    documentation = doc26()
-)
-
-fun checkInRange2(date: MyDate, first: MyDate, last: MyDate): Boolean {
-    todoTask27()
-//    return date in first..last
-}
+fun checkInRange2(date: MyDate, first: MyDate, last: MyDate): Boolean = date in first..last
--- kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/n31Invoke.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/n31Invoke.kt
@@ -1,19 +1,16 @@
 package iii_conventions
 
-import util.TODO
+class Invokable {
+    private var invokeCount = 0
+    operator fun invoke(): Invokable {
+        invokeCount++
+        return this
+    }
 
-
-class Invokable
-
-fun todoTask31(): Nothing = TODO(
-    """
-        Task 31.
-        Change the class 'Invokable' to count the number of invocations:
-        for 'invokable()()()()' it should be 4.
-    """,
-    references = { invokable: Invokable -> })
-
-fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    fun getNumberOfInvocations(): Int {
+        return invokeCount
+    }
 }
+
+fun task31(invokable: Invokable): Int = invokable()()()().getNumberOfInvocations()
+
