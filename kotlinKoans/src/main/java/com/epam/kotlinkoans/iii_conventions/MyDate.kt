9393faf957a12b51c1b9be4af8e0ab8cae4167da
witaless <witaless99@gmail.com>
2019-04-04 02:58:49 +0300

    all parts of kotlin koans finished, all tests passed

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/MyDate.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/MyDate.kt
@@ -1,13 +1,56 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+        }
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate {
+    return addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.n)
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
+    return addTimeIntervals(timeInterval, 1)
+}
+
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val n: Int)
 
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
+
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            private var current = start
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                var next = current
+                current = current.nextDay()
+                return next
+            }
+
+        }
+    }
+
+    operator fun contains(d: MyDate): Boolean {
+        return start <= d && d <= endInclusive
+    }
+}
