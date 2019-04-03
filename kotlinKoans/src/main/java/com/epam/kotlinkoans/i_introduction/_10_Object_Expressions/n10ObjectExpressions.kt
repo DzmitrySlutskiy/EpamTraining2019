430f0a6dddbb3d28fc7f25f17f92d823eaa8f512
AlexGolovach <31997301+alexgolovach@users.noreply.github.com>
2019-04-03 23:41:26 +0300

    Completed i_introduction

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -1,23 +1,7 @@
 package i_introduction._10_Object_Expressions
 
-import util.TODO
-import util.doc10
-import java.util.*
-
-fun todoTask10(): Nothing = TODO(
-    """
-        Task 10.
-        Read about object expressions that play the same role in Kotlin as anonymous classes in Java.
-
-        Add an object expression that provides a comparator to sort a list in a descending order using 'java.util.Collections' class.
-        In Kotlin you use Kotlin library extensions instead of java.util.Collections,
-        but this example is still a good demonstration of mixing Kotlin and Java code.
-    """,
-    documentation = doc10()
-)
-
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    arrayList.sortDescending()
     return arrayList
 }
--- kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
@@ -1,8 +1,5 @@
 package i_introduction._9_Extension_Functions
 
-import util.TODO
-import util.doc9
-
 // declares an extension function that returns the last character
 fun String.lastChar() = this.get(this.length - 1)
 
@@ -15,21 +12,9 @@ fun useExtensionFunction() {
     "abc".lastChar()
 }
 
-// 'lastChar' is compiled to a static function in the class ExtensionFunctionsKt (see JavaCode9.useExtension)
-
-fun todoTask9(): Nothing = TODO(
-    """
-        Task 9.
-        Implement the extension functions Int.r(), Pair<Int, Int>.r()
-        to support the following manner of creating rational numbers:
-        1.r(), Pair(1, 2).r()
-    """,
-    documentation = doc9(),
-    references = { 1.r(); Pair(1, 2).r(); RationalNumber(1, 9) })
-
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
-fun Int.r(): RationalNumber = todoTask9()
-fun Pair<Int, Int>.r(): RationalNumber = todoTask9()
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
 
 
