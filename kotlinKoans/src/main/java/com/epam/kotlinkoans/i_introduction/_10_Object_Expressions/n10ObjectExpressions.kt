66b4748d276b53f087443b3ff37b8678dc6af705
Kirabot <wr30mg@gmail.com>
2019-04-07 20:08:58 +0300

    kotlin homework

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -20,8 +20,8 @@ fun todoTask10(): Nothing = TODO(
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
     Collections.sort(arrayList, object : Comparator<Int> {
-        override fun compare(o1: Int?, o2: Int?): Int {
-            return o2!!.compareTo(o1!!)
+        override fun compare(o1: Int, o2: Int): Int {
+            return o2.compareTo(o1)
         }
     })
     return arrayList
