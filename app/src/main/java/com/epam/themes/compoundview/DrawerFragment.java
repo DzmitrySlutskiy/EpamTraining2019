1332a59a60598fce9326d18fb206945da09dd6fe
wanderer005 <wanderer005@gmail.com>
2019-04-16 11:21:16 +0300

    HW 11.03 Navigation drawer. Fixed - fragment saving argument in bundle

--- app/src/main/java/com/epam/themes/compoundview/DrawerFragment.java
+++ app/src/main/java/com/epam/themes/compoundview/DrawerFragment.java
@@ -2,6 +2,7 @@ package com.epam.themes.compoundview;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,20 +11,38 @@ import android.widget.TextView;
 import com.epam.cleancodetest.R;
 
 public class DrawerFragment extends Fragment {
-    private String pageNumber;
+    private static String PAGE_NUM_KEY = "pageNumber";
+    private int pageNumber;
+
+    public DrawerFragment() {
+        super();
+        pageNumber = 0;
+    }
 
     public static DrawerFragment newInstance(int pageNum) {
         DrawerFragment fragment = new DrawerFragment();
-        fragment.pageNumber = String.valueOf(pageNum);
+        Bundle args = new Bundle();
+        args.putInt(PAGE_NUM_KEY, pageNum);
+        fragment.setArguments(args);
 
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Bundle arguments = getArguments();
+
+        if (arguments != null && arguments.containsKey(PAGE_NUM_KEY)){
+            pageNumber = arguments.getInt(PAGE_NUM_KEY);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_drawer, container, false);
         TextView viewPageNum = view.findViewById(R.id.fragment_label);
-        viewPageNum.setText(getString(R.string.page) + pageNumber);
+        viewPageNum.setText(getString(R.string.page) + String.valueOf(pageNumber));
 
         return view;
     }
